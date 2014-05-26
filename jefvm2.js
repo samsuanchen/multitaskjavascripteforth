7be1c90cff8b180d6e71dd4214f7d98f8c966165
sam <samsuanchen@gmail.com>
2014-05-26 19:32:15 +0800

    fix

--- jefvm2.js
+++ jefvm2.js
@@ -1,42 +1,33 @@
 // <script>
-console.log('begin html')
 'uses strict'
-var fs=require('fs') // file system, fs.readFileSync() could be used
+var fs=require('fs') // for later using fs.readFileSync() and fs.writeFileSync()
 var jeForthVM=require('./jeForthVM.js').jeForthVM
-var jef=new jeForthVM()
+var VM=new jeForthVM()
 var buf='', out=''
 var clrBuf=function(){
-	buf.split(/\r?\n/).map(function(L){
-		if (L.length && L!=='<inp></inp><ok> ok</ok>') {
-			if (jef.directOut.checked)
-				console.log(L)
-			else
-				out+=L+'\n'
-		}
-	})
+	buf=buf.replace(/\r?\n(<inp><\/inp><ok> ok<\/ok>)?\r?\n/g,'\n')
+	if (VM.directOut.checked)	process.stdout.write(buf)
+	else 						out+=buf
 	buf=''
 }
-jef.type=function(x){
+VM.type=function(x){ if (!x) return
 	buf+=x
-	if (buf.match(/\n$/))
+	if (VM.directOut.checked||buf.match(/\n$/))
 		clrBuf()
 }
-jef.directOut={} // jef.tagging=1
-jef.exec(
-	'code . function(){ print(" "+dStk.pop()) }end-code'+	'\n'+
-	'code + function(){ var x=dStk.pop(); dStk[dStk.length-1]+=x }end-code'
-)
-jef.exec('2 3 4 + + .')
+VM.directOut={} // jef.tagging=1
+VM.exec('code . function(){ print(" "+dStk.pop()) }end-code\n'+
+		 'code + function(){ var x=dStk.pop(); dStk[dStk.length-1]+=x }end-code')
+VM.exec('2 3 4 + + .')
 var dir=typeof nwDispatcher==='object'?'..':'.'
-jef.exec(fs.readFileSync(dir+'/topics/basic01').toString())
-jef.exec(fs.readFileSync(dir+'/topics/basic02').toString())
-jef.exec(fs.readFileSync(dir+'/topics/default').toString())
-jef.exec(fs.readFileSync(dir+'/topics/jsFunctionTest').toString())
-jef.exec(fs.readFileSync(dir+'/topics/vocTest').toString())
-jef.exec(fs.readFileSync(dir+'/topics/multiTask').toString())
-if (buf) clrBuf()
-if (out) fs.writeFileSync('./jefVM2.txt',out)
-console.log('./jefVM2.txt length',out.length)
-jef.directOut.checked=1
-jef.exec(fs.readFileSync(dir+'/topics/multiTask1').toString())
+VM.exec(fs.readFileSync(dir+'/topics/basic01').toString())
+VM.exec(fs.readFileSync(dir+'/topics/basic02').toString())
+VM.exec(fs.readFileSync(dir+'/topics/default').toString())
+VM.exec(fs.readFileSync(dir+'/topics/jsFunctionTest').toString())
+VM.exec(fs.readFileSync(dir+'/topics/vocTest').toString())
+VM.exec(fs.readFileSync(dir+'/topics/multiTask').toString())
+if (out+=buf) fs.writeFileSync('./jefVM2.txt',out)
+console.log('write to file ./jefVM2.txt of length',out.length)
+VM.directOut.checked=1
+VM.exec(fs.readFileSync(dir+'/topics/multiTask1').toString())
 // </script>
